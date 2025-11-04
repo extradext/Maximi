21f3097742b2645cb70520e280bb06bf1e168892
extradext <36736464+extradext@users.noreply.github.com>
2025-11-04 17:56:48 -0600

    Update layout.tsx put other </main> main in

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body className={inter.className}>
-<main>
-        {children}
+<main>{children}</main>
       </body>
     </html>
   );
